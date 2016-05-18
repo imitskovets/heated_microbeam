b681d0bd176cabbfc03887a517b69549961a38d8
Ivan Mitskovets <ivan.mitskovets@gmail.com>
2016-05-18 18:37:17 +0300

    simpe video done

--- graph_0.3.py
+++ graph_0.3.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 def updatefig(i):
-    im.set_array(VisualMap[i])
+    im.set_array(Termo[i])
     return im,
-
-N = 1500                                                                                # kol shagov po dt
+alfa = 10
+N_visual = 150                                                                          # kol shagov po dt Visual
 P = 5                                                                                   # pokaz kartinki kazdie P kadrov
-Y_full = 10                                                                             # koll shag po dy
-X_full = 15                                                                             # koll shag po dx
+Y_full = 10 * alfa                                                                             # koll shag po dy
+X_full = 20 * alfa                                                                            # koll shag po dx
+rect_x_start = 3 * alfa
+rect_x_end = 7 * alfa
+rect_y_start = 3 * alfa
+rect_y_end = 5 * alfa
 
 interval = 100
 Writer = animation.writers['ffmpeg']
@@ -18,19 +22,36 @@ fig1 = plt.figure(1)
 
 #
 VisualMap = np.zeros(Y_full * X_full).reshape(1, Y_full, X_full)
-Termo = np.arange(3 * 2 * Y_full * X_full).reshape(3, 2 * Y_full, X_full)
+Termo = np.zeros(Y_full * X_full * N_visual).reshape(N_visual, Y_full, X_full)
 
-for n in np.arange(N):
-    now = n%3
-    if n in np.arange(0, N, P):
-        VisualMap = np.vstack((VisualMap, Termo[now, ::2, :].reshape(1, Y_full, X_full)))
-print(Termo[1])
+#print(VisualMap)
+
+pi = np.arccos(-1)
+print(pi)
+print(np.sin(2*3.14*3 / 5))
+
+for n in np.arange(N_visual):
+    for x in np.arange(X_full):
+        for y in np.arange(Y_full):
+            if x < rect_x_start:
+                Termo[n, y, x] = np.sin(2 * pi * (x + n) / X_full) + np.cos(2 * pi *(y - n) / Y_full)
+            if x > rect_x_end:
+                Termo[n, y, x] = np.sin(2 * pi * (x + n) / X_full) + np.cos(2 * pi * (y - n) / Y_full)
+            if y < rect_y_start:
+                Termo[n, y, x] = np.sin(2 * pi * (x + n) / X_full) + np.cos(2 * pi * (y - n) / Y_full)
+            if y > rect_y_end:
+                Termo[n, y, x] = np.sin(2 * pi * (x + n) / X_full) + np.cos(2 * pi * (y - n) / Y_full)
+            if (y <= rect_y_end) and (y >= rect_y_start) and (x >= rect_x_start) and (x <= rect_x_end):
+                Termo[n, y, x] = 1
+    Termo[n, 0, 0] = 1
+print(Termo)
 #
 
 setka = np.arange(Y_full * X_full).reshape(Y_full, X_full)
+#WTF????????????????
 im = plt.imshow(np.sin(setka))
 
 plt.figure(1)
-kino = animation.FuncAnimation(fig1, updatefig, np.arange(int(N/P)), interval = interval)
+kino = animation.FuncAnimation(fig1, updatefig, np.arange(int(N_visual / P)), interval = interval)
 #fig.show()
 kino.save('Video.mp4', writer = writer)
